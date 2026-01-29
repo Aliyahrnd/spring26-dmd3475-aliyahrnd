5a3847d8fcc01e69cc0e53747b0c81b45778f923
Aliyahrnd <aliyah.ronda@uconn.edu>
2026-01-29 11:50:37 -0500

    this is my week 2 code

---
+++ week-02/script.js
@@ -0,0 +1,24 @@
+var week = 2;
+
+console.log("Hello, This is week " + week + ".");
+
+
+//alert("Hello, this is really week 2! :)");
+var today = "Thursday";
+//console.log("today");
+var tomorrow = "Friday";
+
+//console.log("Today is Thursday.");
+//console.log("Tomorrow is Friday.");
+
+var greeting = "Today is " + today + ".";
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+console.log("Next week will be week " + nextWeek + ".");
+
+//var title = "Joel";
+
+var title = prompt("What is your name?");
+alert("My name is " + title + ".");
